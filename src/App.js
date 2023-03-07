8425e2015819189274937b62f2679b2656955664
YusufYolcuu <yyolcu634@gmail.com>
2023-03-07 13:11:50 +0300

    routes & pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./scss/style.scss";
+import React, { Suspense } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import HomeLayout from "./layout/HomeLayout";
+
+// import "./scss/style.scss";
+
+const Login = React.lazy(() => import("./views/pages/login/Login"));
+// const Register = React.lazy(() => import("./views/pages/register/Register"));
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <HashRouter>
+      {
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<HomeLayout />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Suspense>
+      }
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/views/pages/login/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from "@coreui/react";
+
+const Login = () => {
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={8}>Hello Login</CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  );
+};
+
+export default Login;
