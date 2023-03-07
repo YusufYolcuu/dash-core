768cff4a574a13f49f1525c0936c37a00d0ede8a
YusufYolcuu <yyolcu634@gmail.com>
2023-03-07 18:42:08 +0300

    sidebar item data and component

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,7 +1,23 @@
+import { CSidebar, CSidebarBrand, CSidebarNav } from "@coreui/react";
 import React from "react";
+import SimpleBar from "simplebar-react";
+import navigation from "../_nav";
+import AppSidebarNav from "./AppSidebarNav";
 
 const AppSidebar = () => {
-  return <div>AppSidebar</div>;
+  return (
+    <CSidebar position="fixed">
+      <CSidebarBrand className="d-none d-md-flex" to="/">
+        {" "}
+        DashCore
+      </CSidebarBrand>
+      <CSidebarNav>
+        <SimpleBar>
+          <AppSidebarNav items={navigation} />{" "}
+        </SimpleBar>
+      </CSidebarNav>
+    </CSidebar>
+  );
 };
 
 export default AppSidebar;
